70a42f6f39a0ab7b7cef274230c16a2b73053450
Piyush Sati <69719134+Pinqua@users.noreply.github.com>
2022-01-10 23:24:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withPWA = require("next-pwa");
+const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === "development",
     dest: "public",
+    runtimeCaching,
   },
   env: {
     stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
